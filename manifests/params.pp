aa5e081229e1f3733544391f007f09476991f5d1
supercow <james.sweeny@gmail.com>
2015-02-15 21:03:55 -0500

    Update CentOS version

--- manifests/params.pp
+++ manifests/params.pp
@@ -42,8 +42,8 @@ class plexmediaserver::params {
           $plex_pkg = 'plexmediaserver-0.9.9.12.504-3e7f93c.i386.rpm'
         }
         default : {
-          $plex_url = 'http://downloads.plexapp.com/plex-media-server/0.9.9.12.504-3e7f93c/plexmediaserver-0.9.9.12.504-3e7f93c.x86_64.rpm'
-          $plex_pkg = 'plexmediaserver-0.9.9.12.504-3e7f93c.x86_64.rpm'
+          $plex_url = 'https://downloads.plex.tv/plex-media-server/0.9.11.7.803-87d0708/plexmediaserver-0.9.11.7.803-87d0708.x86_64.rpm' 
+          $plex_pkg = 'plexmediaserver-0.9.11.7.803-87d0708.x86_64.rpm'
         }
       }
       $plex_provider = 'rpm'
