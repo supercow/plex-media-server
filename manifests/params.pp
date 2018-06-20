398fe46ef6d2e1c93fa8200935318f93334270b6
supercow <james.sweeny@gmail.com>
2018-06-20 07:49:24 -0400

    Merge back WhatsARanjit updates

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,41 +2,61 @@ class plexmediaserver::params {
   # Get download URL
   case $::operatingsystem {
     'Darwin': {
-      $plex_url      = 'https://downloads.plex.tv/plex-media-server/0.9.12.19.1537-f38ac80'
-      $plex_pkg      = 'PlexMediaServer-0.9.12.19.1537-f38ac80-OSX.zip'
+      $plex_url      = 'http://downloads.plexapp.com/plex-media-server/0.9.9.12.504-3e7f93c/PlexMediaServer-0.9.9.12.504-3e7f93c-OSX.zip'
+      $plex_pkg      = 'PlexMediaServer-0.9.9.12.504-3e7f93c-OSX.zip'
       $plex_provider = 'pkgdmg'
     }
     'Ubuntu': {
-      $plex_url         = 'https://downloads.plex.tv/plex-media-server/0.9.12.19.1537-f38ac80'
-      $plex_pkg         = "plexmediaserver_0.9.12.19.1537-f38ac80_${::architecture}.deb"
-      $plex_provider    = 'dpkg'
+      case $::architecture {
+        'i386': {
+          $plex_url = 'http://downloads.plexapp.com/plex-media-server/0.9.9.12.504-3e7f93c/plexmediaserver_0.9.9.12.504-3e7f93c_i386.deb'
+          $plex_pkg = 'plexmediaserver_0.9.9.12.504-3e7f93c_i386.deb'
+        }
+        default : {
+          $plex_url = 'http://downloads.plexapp.com/plex-media-server/0.9.9.12.504-3e7f93c/plexmediaserver_0.9.9.12.504-3e7f93c_amd64.deb'
+          $plex_pkg = 'plexmediaserver_0.9.9.12.504-3e7f93c_amd64.deb'
+        }
+      }
+      $plex_provider = 'dpkg'
       $plex_ubuntu_deps = [ 'libavahi-core7', 'libdaemon0', 'avahi-daemon' ]
-      $plex_config      = '/etc/default/plexmediaserver'
+      $plex_config = '/etc/default/plexmediaserver'
     }
     'Fedora': {
-      $plex_url      = 'https://downloads.plex.tv/plex-media-server/0.9.12.19.1537-f38ac80'
-      $plex_pkg      = "plexmediaserver-0.9.12.19.1537-f38ac80.${::architecture}.rpm"
+      case $::architecture {
+        'i386': {
+          $plex_url = 'http://downloads.plexapp.com/plex-media-server/0.9.9.12.504-3e7f93c/plexmediaserver-0.9.9.12.504-3e7f93c.i386.rpm'
+          $plex_pkg = 'plexmediaserver-0.9.9.12.504-3e7f93c.i386.rpm'
+        }
+        default : {
+          $plex_url = 'http://downloads.plexapp.com/plex-media-server/0.9.9.12.504-3e7f93c/plexmediaserver-0.9.9.12.504-3e7f93c.x86_64.rpm'
+          $plex_pkg = 'plexmediaserver-0.9.9.12.504-3e7f93c.x86_64.rpm'
+        }
+      }
       $plex_provider = 'rpm'
       $plex_config   = '/etc/sysconfig/PlexMediaServer'
     }
     'CentOS': {
-      $plex_url      = 'https://downloads.plex.tv/plex-media-server/0.9.12.19.1537-f38ac80'
-      $plex_pkg      = "plexmediaserver-0.9.12.19.1537-f38ac80.${::architecture}.rpm"
+      case $::architecture {
+        'i386': {
+          $plex_url = 'http://downloads.plexapp.com/plex-media-server/0.9.9.12.504-3e7f93c/plexmediaserver-0.9.9.12.504-3e7f93c.i386.rpm'
+          $plex_pkg = 'plexmediaserver-0.9.9.12.504-3e7f93c.i386.rpm'
+        }
+        default : {
+          $plex_url = 'https://downloads.plex.tv/plex-media-server/0.9.11.7.803-87d0708/plexmediaserver-0.9.11.7.803-87d0708.x86_64.rpm' 
+          $plex_pkg = 'plexmediaserver-0.9.11.7.803-87d0708.x86_64.rpm'
+        }
+      }
       $plex_provider = 'rpm'
       $plex_config   = '/etc/sysconfig/PlexMediaServer'
     }
     default: { fail("${::operatingsystem} is not supported by this module.") }
   }
-
+  # Whether or not to manage the staging module
+  $plex_staging = false
   # General stuff
   $plex_user                                       = 'plex'
   $plex_media_server_home                          = '/usr/lib/plexmediaserver'
-  $plex_media_server_application_support_dir_array = [
-    '`getent passwd $PLEX_USER|awk -F : \'{print $6}\'`',
-    '/Library/Application Support'
-  ]
-  $plex_media_server_application_support_dir =
-    join($plex_media_server_application_support_dir_array, '')
+  $plex_media_server_application_support_dir ='`getent passwd $PLEX_USER|awk -F : \'{print $6}\'`/Library/Application Support'
   $plex_media_server_max_plugin_procs        = '6'
   $plex_media_server_max_stack_size          = '10000'
   $plex_media_server_max_lock_mem            = '3000'
